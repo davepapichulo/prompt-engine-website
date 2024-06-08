bbe256dc2e08af8e06a13cf4b1355c0171c7e816
davepapichulo <davidasukelly@gmail.com>
2024-06-08 14:01:40 +0100

    added mobile dropdown view

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -8,6 +8,7 @@ import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
 
 const Navbar = () => {
+
   const isUserLoginIn = true;
   const [providers, setProviders] = useState(null);
   useEffect(() => {
@@ -17,7 +18,9 @@ const Navbar = () => {
         setProviders(response);
       }
   }, [ ])
+const [ToggleDropDown, setToggleDropDown] = useState(false);
 
+  
   return (
    <nav className='flex-between w-full mb-16 pt-5'>
       <Link href ='/' className='flex items-center gap-2'>
@@ -49,8 +52,43 @@ const Navbar = () => {
           </>
         )
       }
+      </div>
+
+      <div className='sm:hidden flex relative'>
+        {isUserLoginIn ? (
+          <div>
+            <Image src= '/assets/images/logo.svg' alt='logo' height={35} width= {35} className='object-contain' 
+            onClick={() => setToggleDropDown((prev)=> !prev )}/>
+
+            {ToggleDropDown && (
+              <div className='dropdown'>
+                  <Link href='/profile' className='dropdown_link' onClick={()=>setToggleDropDown(false)}>
+                    My Profile
+                  </Link>
+                  <Link href='/create-prompt' className='dropdown_link' onClick={()=>setToggleDropDown(false)}>
+                    Create Prompt
+                  </Link>
+                  <button type='button' onClick = {() => { setToggleDropDown(false); signOut()}} className='mt-5 w-full black_btn'>
+                      Sign Out
+                  </button>
+              </div>
+              
+            )}
+          </div>
+        ):(
+          <>
+            {providers && Object.values(providers).map((providers) => (
+              <button type='button' key={provider.name} onClick={() => signIn(provider.id)} className='black_btn'>
+                Sign In
+              </button>
+              
+            ))}
+          </>
+        )}
 
       </div>
+
+
    </nav>
   )
 }
